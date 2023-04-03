1b0219415fb50f90b910de081ce440280a24a0f3
Alexandre Delmotte <alex.delmotte89@gmail.com>
2023-04-03 12:27:20 +0200

    update ws

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ProgramRepository::class)]
+#[UniqueEntity(('title'), message: 'ce titre existe déjà')]
 class Program
 {
     #[ORM\Id]
@@ -17,9 +20,11 @@ class Program
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(max: 255)]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: 'Ce champ ne doit pas être vide')]
     private ?string $synopsis = null;
 
     #[ORM\Column(length: 255, nullable: true)]
